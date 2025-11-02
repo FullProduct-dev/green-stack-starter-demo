3c39822ecb50f24f1cce3844e29364371ee32bda
codinsonn <thorr@codinsonn.dev>
2025-11-02 19:20:02 +0100

    fix: Update collect-schemas script to collect partial definitions

---
+++ packages/@registries/schemas.partials.ts
@@ -0,0 +1,5 @@
+// -i- Auto generated with "npx turbo run @green-stack/core#collect:schemas"
+
+import { z } from '@green-stack/schemas'
+/* --- Exports --------------------------------------------------------------------------------- */
+export const partials = {} as Record<string, z.ZodRawShape>
